cb883eba8e259639423742d84730f8fcfb58d2c3
Krishna Bollineni <53118174+Krish23-collab@users.noreply.github.com>
2022-05-14 22:37:18 +1000

    Add files via upload

---
+++ src/lib/connectMongo.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { USER_NAME, PASSWORD } from "../config";
+
+const username = encodeURIComponent(USER_NAME);
+const password = encodeURIComponent(PASSWORD);
+const dataBaseName = "contacts-app";
+const MONGODB_URI = `mongodb+srv://${username}:${password}@cluster0.9hw4g.mongodb.net/${dataBaseName}?retryWrites=true&w=majority`;
+
+console.log(MONGODB_URI);
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectMongo() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
+
+export default connectMongo;
